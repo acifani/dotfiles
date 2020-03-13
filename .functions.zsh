6b8133590ef3c0b94f7daa39c6205a68e03334d0
Alessandro Cifani <alessandro.cifani@scmitaly.it>
2020-03-13 15:25:40 +0100

    add kitty conf

--- .functions.zsh
+++ .functions.zsh
@@ -11,3 +11,13 @@ weather() {
 hdi() {
     howdoi $* -c -n 5;
 }
+
+listening() {
+    if [ $# -eq 0 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P
+    elif [ $# -eq 1 ]; then
+        sudo lsof -iTCP -sTCP:LISTEN -n -P | grep -i --color $1
+    else
+        echo "Usage: listening [pattern]"
+    fi
+}
