c3906620ad91e351b8328652b41497578b6a9bcf
Alessandro Cifani <alessandro.cifani@gmail.com>
2025-05-15 12:08:57 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,6 @@ git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 
 # Add asdf plugins
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add python https://github.com/danhper/asdf-python.git
+asdf plugin add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin add python https://github.com/danhper/asdf-python.git
